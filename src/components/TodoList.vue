4acf81dd496d82960fe1acaed8825352e6ec4901
kbyyd24 <melo@gaoyuexiang.cn>
2018-08-24 14:36:02 +0800

    add my first component

---
+++ src/components/TodoList.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="todoList">
+        <ol>
+            <li v-for="todo in todos" :key="todo.id">{{ todo.content }}</li>
+        </ol>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'TodoList',
+  data: function() {
+    return {
+      todos: [
+        {
+          id: 1,
+          content: 'First Todo'
+        },
+        {
+          id: 2,
+          content: 'Can you see me'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+.todoList {
+    width: 40%;
+    margin: auto;
+}
+</style>
