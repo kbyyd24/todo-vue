5bc028c57b33ebfdbb7739658fb07b1c499811ea
kbyyd24 <melo@gaoyuexiang.cn>
2018-12-09 16:51:06 +0800

    [contract test] set up pact

---
+++ pactSetup.js
@@ -0,0 +1,14 @@
+import { resolve } from 'path';
+import { Pact } from '@pact-foundation/pact';
+​
+global.port = 8989;
+global.provider = new Pact({
+  port: global.port,
+  log: resolve(process.cwd(), 'logs', 'mockserver-integration.log'),
+  dir: resolve(process.cwd(), 'pacts'),
+  spec: 2,
+  cors: true,
+  pactfileWriteMode: 'update',
+  consumer: 'todo app vue consumer',
+  provider: 'todo app provider'
+});
